381c4a4ec0cf49f6c61bfafb47134f18b52f9462
abdun_jos <nafichasani@gmail.com>
2025-02-21 13:31:05 +0700

    fix: main

--- src/main.js
+++ src/main.js
@@ -3,11 +3,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { createHead, useHead } from '@vueuse/head'
+import { createHead, HeadVuePlugin } from '@vueuse/head'
 
 const app = createApp(App)
 const head = createHead()
+app.use(HeadVuePlugin, head)
+app.use(head)
 
 app.use(router)
-app.use(head)
 app.mount('#app')
